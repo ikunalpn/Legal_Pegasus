c9b6870e7ab813cb7abe0b44be8d615f0a275ce2
Kunal Pandhare <kunalpn7@gmail.com>
2025-01-30 19:03:38 +0530

    summary length adjusted

--- utils/legal_pegasus.py
+++ utils/legal_pegasus.py
@@ -1,16 +1,12 @@
-import os
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import pdfplumber
-
-transformers_cache_dir = os.environ.get("TRANSFORMERS_CACHE", "/var/task/cache")
-
-tokenizer = AutoTokenizer.from_pretrained("akhilm97/pegasus_indian_legal", cache_dir=transformers_cache_dir)
-model = AutoModelForSeq2SeqLM.from_pretrained("akhilm97/pegasus_indian_legal", cache_dir=transformers_cache_dir)
 
 def extract_text_from_pdf(pdf_file):
     with pdfplumber.open(pdf_file) as pdf:
         text = "\n".join([page.extract_text() for page in pdf.pages])
     return text
+tokenizer = AutoTokenizer.from_pretrained("akhilm97/pegasus_indian_legal")
+model = AutoModelForSeq2SeqLM.from_pretrained("akhilm97/pegasus_indian_legal")
 
 def summarize_text(text):
     input_tokenized = tokenizer.encode(text, return_tensors='pt', max_length=1024, truncation=True)
@@ -19,7 +15,7 @@ def summarize_text(text):
                                 no_repeat_ngram_size=3,
                                 length_penalty=2.0,
                                 min_length=150,
-                                max_length=250,
+                                max_length=500,
                                 early_stopping=True)
     summary = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids][0]
     return summary
